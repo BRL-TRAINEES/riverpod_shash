a35f862dbe1ad8468964ecb49e19f0091c26c3cc
ShashwatXD <legendshashwat.gkp@gmail.com>
2024-10-20 19:41:01 +0530

    river 2.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -12,12 +12,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Homepage(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      home: HomePage(),
     );
   }
 }
+
+
+class HomePage extends ConsumerWidget{
+  HomePage ({Key? key}):super(key:key);
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white, 
+  
+        title:Text('HomePage',style: TextStyle(color: Colors.black),),
+      ),
+    );
+  }
+  }
+
 
