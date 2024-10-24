2650370e6b59727622f3ec429f9ef9f57d5488c9
ShashwatXD <legendshashwat.gkp@gmail.com>
2024-10-25 00:37:40 +0530

    notifier

--- lib/api.dart
+++ lib/api.dart
@@ -9,7 +9,11 @@ class ApiService {
     final response= await http.get(Uri.parse('https://v6.exchangerate-api.com/v6/71478dfdd9bb2533b494ba46/latest/USD'));
     if(response.statusCode==200){
     final data=jsonDecode(response.body);
-    return data['conversion_rates']['INR'];}
+    double inrRate = data['conversion_rates']['INR'];
+    String lastUpdated = data['time_last_update_utc'];
+
+    return inrRate;
+    }
     else{
       throw Exception('Failed to fetch data');
     }
--- lib/home.dart
+++ lib/home.dart
@@ -11,10 +11,14 @@ final TextEditingController uscon = TextEditingController();
 
 class CurrencyConvert extends ConsumerWidget {
   CurrencyConvert({super.key});
+  final convertedProvider = StateProvider<double>((ref) => 0.0);
+  
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final currencyValue=ref.watch(currencyProvider);
+        final convertedAmount = ref.watch(convertedProvider);
+
 
     return Scaffold(
               backgroundColor: Color.fromARGB(255, 80, 79, 79),
@@ -27,6 +31,15 @@ class CurrencyConvert extends ConsumerWidget {
         padding: const EdgeInsets.all(10.0),
         child: Column(
           children: [
+            
+
+            currencyValue.when(
+           data: (fetchedINR) => Text('Currently: 1 USD = $fetchedINR INR',style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            loading: () =>CircularProgressIndicator(),
+             error: (error, stack) => Text('Error fetching rate',style: TextStyle(color: Colors.red),
+  ),
+),
 
             const SizedBox(height: 215),
             TextField(
@@ -41,6 +54,33 @@ class CurrencyConvert extends ConsumerWidget {
                 fillColor: Color.fromRGBO(30, 30, 30, 1),
               ),
               style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)), 
-            )
-            ]),
-          ));}}
+            ),//end of textfield--
+
+            SizedBox(height:10),
+            
+            ElevatedButton(
+              style:ElevatedButton.styleFrom(backgroundColor: Colors.grey),
+              onPressed: () {
+                double usd =double.parse(uscon.text);               
+               double fetchedINR = 1; 
+
+                if (currencyValue.hasValue) {
+                fetchedINR = currencyValue.value!; 
+                  }
+
+                 
+                 double convertedAmount = usd * fetchedINR;
+                ref.read(convertedProvider.notifier).state = convertedAmount; //works similiAR to  setstate
+
+
+              },
+              child: Text('Convert',style:TextStyle(color: const Color.fromARGB(255, 72, 41, 132))),
+            ),
+            SizedBox(height: 20),
+            Text('Converted Amount: ${convertedAmount} INR', style: TextStyle(color: Colors.white)),
+            ]
+            
+            
+            ),
+          ));
+          }}
